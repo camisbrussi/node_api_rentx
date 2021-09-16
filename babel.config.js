02a318c0dd5558ba6b362b4f2468cac6610747b2
camisbrussi <camila.sbrussi@universo.univates.br>
2021-09-16 10:47:52 -0300

    Babel

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,6 @@
-module.export = {
+module.exports = {
   presets: [
-    ["@babel/preset-env", {
-      targets: { node: current }
-    }],
+    ["@babel/preset-env", { targets: { node: "current" } }],
     "@babel/preset-typescript",
   ],
   plugins: [
@@ -10,16 +8,18 @@ module.export = {
       "module-resolver",
       {
         alias: {
-          "@modules": ["./src/modules"],
-          "@config": ["./src/config"],
-          "@shared": ["./src/shared"],
-          "@errors": ["./src/errors"],
-          "@utils": ["./src/utils"]
-        },
-      },
+          "@modules": "./src/modules",
+          "@config": "./src/config",
+          "@shared": "./src/shared",
+          "@errors": "./src/errors",
+          "@utils": "./src/utils"
+        }
+      }
     ],
     "babel-plugin-transform-typescript-metadata",
-    ["@babel/plugin-proposal-decorators", { legacy: true }]
-    ["@babel/plugin-proposal-class-properties", { loose: true }]
-  ],
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ["@babel/plugin-proposal-private-methods", { "loose": true }]
+
+  ]
 }
